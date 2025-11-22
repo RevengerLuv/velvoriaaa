ca5f93ed7bad9e4e5ecd1c4b466fe769381b1c89
RevengerLuv <luvv.kumar.755@gmail.com>
2025-11-23 00:22:39 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { HashRouter } from 'react-router-dom' // CHANGE TO HashRouter
-import App from './App.jsx'
-import { AppContextProvider } from './context/AppContext.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { HashRouter } from "react-router-dom"; // CHANGE TO HashRouter
+import { AppContextProvider } from "./context/AppContext.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <HashRouter> {/* CHANGE FROM BrowserRouter TO HashRouter */}
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <HashRouter> {/* CHANGE TO HashRouter */}
       <AppContextProvider>
         <App />
       </AppContextProvider>
     </HashRouter>
-  </React.StrictMode>,
-)
+  </StrictMode>
+);
