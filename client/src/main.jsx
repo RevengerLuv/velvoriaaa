8a1a52a963067de2743af5963884268b86ad2431
RevengerLuv <luvv.kumar.755@gmail.com>
2025-11-23 00:18:19 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 import App from './App.jsx'
-import { AppContextProvider } from './context/AppContext.jsx' // ADD THIS
+import { AppContextProvider } from './context/AppContext.jsx' // IMPORT CONTEXT PROVIDER
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HashRouter>
-      <AppContextProvider> {/* WRAP YOUR APP WITH CONTEXT PROVIDER */}
+      <AppContextProvider> {/* WRAP APP WITH CONTEXT PROVIDER */}
         <App />
       </AppContextProvider>
     </HashRouter>
