7fbc29d4c597b0a02d5b8d659dd4623ed80b8269
RevengerLuv <luvv.kumar.755@gmail.com>
2025-11-22 22:45:25 +0530

    Update address.routes.js

--- backend/routes/address.routes.js
+++ backend/routes/address.routes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import authUser from "../middlewares/authUser.js";
+import { authSeller } from "../../middlewares/authSeller.js";
 import { addAddress, getAddress } from "../controller/address.controller.js";
 
 const router = express.Router();
