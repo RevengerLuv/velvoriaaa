774d1b88af837487669f3dc37d13f0909be21fe6
RevengerLuv <luvv.kumar.755@gmail.com>
2025-11-22 22:46:00 +0530

    Update cart.routes.js

--- backend/routes/cart.routes.js
+++ backend/routes/cart.routes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { updateCart } from "../controller/cart.controller.js";
+import { authSeller } from "../../middlewares/authSeller.js";
 import authUser from "../middlewares/authUser.js";
 
 const router = express.Router();
